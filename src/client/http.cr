bf19b5ac636cebdbd0e016fe7944697559f85744
Scott McClung <scott@nerdmagik.com>
2023-12-28 19:25:40 -0800

    Raise exception when api returns error response

--- src/client/http.cr
+++ src/client/http.cr
@@ -81,11 +81,29 @@ module SurrealDB
 
     private def request(method : String, endpoint : String, query : String?)
       res = @client.exec method, endpoint, headers: @headers, body: query
+      process_request(res)
+    end
 
-      begin
-        Array(Response).from_json res.body
-      rescue ex
-        ErrorResponse.from_json res.body
+    private def process_request(response : HTTP::Client::Response)
+      case response.status_code
+      when 200..299
+        # For cases when "status" is one of the following:
+        #   OK
+        return Array(Response).from_json res.body if ["OK"].includes?(Response.from_json(response.body).status)
+
+        raise Client::ServerError.new(ErrorResponse.from_json(response.body))
+      when 400
+        raise Client::ServerError.new("400: Server Not Found")
+      when 500
+        raise Client::ServerError.new("500: Internal Server Error")
+      when 502
+        raise Client::ServerError.new("502: Bad Gateway")
+      when 503
+        raise Client::ServerError.new("503: Service Unavailable")
+      when 504
+        raise Client::ServerError.new("504: Gateway Timeout")
+      else
+        raise Client::ServerError.new("Server returned error #{response.status_code}")
       end
     end
   end
